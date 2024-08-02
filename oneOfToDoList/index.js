040ad5490389904810d0c55122f93b7501c7372a
pinK0der <obsidianfury64@gmail.com>
2024-08-02 12:42:07 +0100

    second version of to do list

--- oneOfToDoList/index.js
+++ oneOfToDoList/index.js
@@ -14,3 +14,37 @@ buttonAdd.addEventListener("mouseout", function()
     this.classList.add("buttonDownAnimation");
 }
 );
+
+const inputBox = document.getElementById("inputBox");
+const tasksList = document.getElementById("tasksList");
+
+function addTask()
+{
+    if(inputBox.value === '')
+    {
+        alert("Please enter a task");
+    }
+    else
+    {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        tasksList.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+
+tasksList.addEventListener("click", function(e)
+{
+    if(e.target.tagName === "LI")
+    {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN")
+    {
+        e.target.parentNode.remove();
+    }
+}, false);
