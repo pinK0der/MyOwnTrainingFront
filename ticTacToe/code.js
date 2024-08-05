29529397080602df77ee21ecf10a19b729cb3191
pinK0der <obsidianfury64@gmail.com>
2024-08-05 12:19:47 +0100

    static tic tac toe

---
+++ ticTacToe/code.js
@@ -0,0 +1,5 @@
+let buttonRefer = document.querySelectorAll(".button-opt");
+let messageReference = document.querySelector(".idkMess");
+let newGameButton = document.getElementsByClassName("newGame");
+let restartButton = document.getElementsByClassName("restart");
+let h2Reference = document.getElementById("mess");
