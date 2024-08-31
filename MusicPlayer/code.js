bff4339641aa8638f2cfef5cfdf6341c3eecf2c0
pinK0der <obsidianfury64@gmail.com>
2024-08-31 20:54:38 +0100

    added changing features like volume...

--- MusicPlayer/code.js
+++ MusicPlayer/code.js
@@ -1,6 +1,4 @@
 const audio = document.getElementById('audio');
-const playButton = document.getElementById('playButton');
-
 const progress = document.querySelector('.progress');
 
 audio.addEventListener('timeupdate', updateProgress);
@@ -13,17 +11,46 @@ function updateProgress() {
     }
 }
 
-let playing = false;
+function togglePlay() {
+    const playIcon = document.getElementById('playIcon');
 
-function togglePlay()
+    if (audio.paused) {
+        audio.play(); // Почати відтворення аудіо
+        playIcon.src = 'img/continue.png'; // Змінити іконку на паузу
+    } else {
+        audio.pause(); // Поставити аудіо на паузу
+        playIcon.src = 'img/pause.png'; // Змінити іконку на відтворення
+    }
+}
+
+const volumeSlider = document.getElementById('volumeSlider');
+audio.volume = volumeSlider.value / 100;
+
+volumeSlider.addEventListener('input', function () {
+    const volume = volumeSlider.value / 100;
+    audio.volume = volume;
+});
+
+const progressContainer = document.getElementById("progressContainer");
+const progressChanging = document.getElementById("progress");
+
+audio.addEventListener('timeupdate', updateProgress);
+
+progressContainer.addEventListener('click', setProgress);
+
+function updateProgress()
 {
-    if (playing)
-    {
-        audio.pause();
-    }
-    else
-    {
-        audio.play();
-    }
-    playing = !playing;
+    const duration = audio.duration;
+    const currentTime = audio.currentTime;
+    const progressPercent = (currentTime / duration) * 100;
+    progressChanging.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e)
+{
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
 }
